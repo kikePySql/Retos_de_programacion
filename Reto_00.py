125f5be23c1c55b690ef31df999e0c670a44df38
kikePySql <kike10a2013@gmail.com>
2025-05-31 19:08:37 -0500

    Implements initial Python challenge solution
    Implements a solution for the initial programming challenge.
    
    The solution includes:
    - Adding a comment with the official Python website URL.
    - Demonstrating comment syntax.
    - Creating variables and a pseudo-constant.
    - Defining variables representing primitive data types.
    - Printing a greeting message.

---
+++ Reto_00.py
@@ -0,0 +1,40 @@
+"""
+¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+"""
+
+# URL: https://www.python.org
+
+my_variable = "Hola mundo"
+my_constante = "En python no se pueden crear constantes como tal" #toca tenerla por convencion
+
+str = "Enrique"
+int = 10
+float = 3.1416
+complex = (1 + 1j)
+bool = True
+list = [1, 2, 3, 4, 5]
+dict = {"name":"Enrique", "surname":"Almanza", "age": 27}
+tuple = (1, 2, 3)
+zip = zip([1,2],[3,4]) 
+
+print("Hola, Python")
